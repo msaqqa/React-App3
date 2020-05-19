ad532bd73cf9a7f024b730b609dcf2161541fc58
msaqqa <42069984+msaqqa@users.noreply.github.com>
2020-05-19 19:23:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component{
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api = await fetch (`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=e36ed364400282e43250b6c4c0274d44`)
+    const api = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=e36ed364400282e43250b6c4c0274d44`)
     const data = await api.json();
     if(city && country){
       this.setState({
